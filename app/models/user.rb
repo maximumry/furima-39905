b6c8f020097426fd12c94ccab3d82c1be2ef3f35
maximumry <teapots-thorium.07@icloud.com>
2023-09-30 12:25:34 +0900

    ログインログアウト新規

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: { message: "can't be blank"}
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "is invalid. Include both letters and numbers" }
+  validates :last_name, presence: { message: "can't be blank"}, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters" }
+  validates :first_name, presence: { message: "can't be blank"}, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters" }
+  validates :kana_last, presence: { message: "can't be blank"}, format: { with: /\A[ァ-ンーー]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :kana_first, presence: { message: "can't be blank"}, format: { with: /\A[ァ-ンーー]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :birth_day, presence: { message: "can't be blank"}
 end
