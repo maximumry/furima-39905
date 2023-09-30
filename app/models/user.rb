c324750ebb2673f5a25f0aa3f57a7c074a9d7c7e
maximumry <teapots-thorium.07@icloud.com>
2023-09-30 18:18:03 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: { message: "can't be blank"}
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "is invalid. Include both letters and numbers" }
-  validates :last_name, presence: { message: "can't be blank"}, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters" }
-  validates :first_name, presence: { message: "can't be blank"}, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters" }
-  validates :kana_last, presence: { message: "can't be blank"}, format: { with: /\A[ァ-ンーー]+\z/, message: "is invalid. Input full-width katakana characters" }
-  validates :kana_first, presence: { message: "can't be blank"}, format: { with: /\A[ァ-ンーー]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :last_name, presence: { message: "can't be blank"}, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+  validates :first_name, presence: { message: "can't be blank"}, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+  validates :kana_last, presence: { message: "can't be blank"}, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :kana_first, presence: { message: "can't be blank"}, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :birth_day, presence: { message: "can't be blank"}
 end
