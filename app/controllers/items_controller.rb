944f36eb9e093ab29816966d71a7dbbc8760f265
maximumry <teapots-thorium.07@icloud.com>
2023-10-02 18:28:41 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,5 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:image, :name, :info, :category_id, :condition_id, :burden_info_id, :prefecture_id, :shipping_day_id. :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :info, :category_id, :condition_id, :burden_info_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,9 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   has_one_attached :image
 
-  validates :name, :info, :price, presence: true
-
+  validates :image, presence: { message: "can't be blank"}
+  validates :name, presence: { message: "can't be blank"}
+  validates :info, presence: { message: "can't be blank"}
   validates :category_id, :condition_id, :burden_info_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :price, presence: { message: "can't be blank" }, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range" }
 end
