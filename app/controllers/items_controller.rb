2ca821a103ab9b975ed591997ebafcf4eabe1138
maximumry <teapots-thorium.07@icloud.com>
2023-10-04 17:30:26 +0900

    update Actionの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def move_to_index
     unless user_signed_in?
