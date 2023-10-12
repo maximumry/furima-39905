2cbafa511c948ee940ccc705f7bbb8d3e20fba2c
maximumry <teapots-thorium.07@icloud.com>
2023-10-12 15:49:46 +0900

    自動デプロイの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,3 +91,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+server '3.114.253.156', user: 'ec2-user', roles: %w{app db web}
